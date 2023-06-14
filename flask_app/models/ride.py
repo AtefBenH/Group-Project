ae9199eec223dab343ecbd04134f945176ea90d7
AtefBenH <ben_hasnaoui_atef@yahoo.fr>
2023-06-14 23:14:29 +0100

    12HoursMargeAdded

--- flask_app/models/ride.py
+++ flask_app/models/ride.py
@@ -152,7 +152,7 @@ class Ride:
             JOIN users ON rides.user_id = users.id 
             WHERE rides.from_location LIKE %(from_location)s 
             AND rides.to_location LIKE %(to_location)s
-            AND rides.when_time BETWEEN (%(when_time)s - INTERVAL '2' HOUR) AND (%(when_time)s + INTERVAL '2' HOUR)
+            AND rides.when_time BETWEEN (%(when_time)s - INTERVAL '12' HOUR) AND (%(when_time)s + INTERVAL '12' HOUR)
             AND rides.seats >0;
             """
         results = connectToMySQL(DATABASE).query_db(query, data)
